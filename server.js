2ccc6e44cbeaf66e107d92100f544aae61d80e74
rimathi <rimathivanan@gmail.com>
2017-08-09 12:00:24 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-one', function (req, res) {
+  res.sendFile("This is my first article");
+});
+app.get('/article-two', function (req, res) {
+    res.sendFile("This is my secound article");
+});
+app.get('/article-three', function (req, res) {
+    res.sendFile("This is my third article");
+});
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
