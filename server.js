e1e1612e719b74d2937a53a809bdd04910bcfc62
rimathi <rimathivanan@gmail.com>
2017-08-10 15:44:14 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,15 +19,55 @@ var content={
       This a my first article </p>`
          
     }
+    function createTemplate(data)
+    {
+        var tilte=data.title;
+        var heading=data.heading;
+        var date=data.date;
+        var content=data.content;
+        
+    var htmltemplate=`<html>
+<head>
+    <title>
+          ${title}
+    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link href="/ui/style.css" rel="stylesheet" />
+   
+</head>
+<body>
     
-    var htmltemplate=`
+    <div class="container">
+    <div> 
+    <a href="/">Home</a>
+    </div>
+    <hr/>
+    <h3>
+          ${heading} 
+    </h3>
+    
+    <div>
+        
+    ${date}
+    
+    </div>
+    
+    <div>
+        ${content}
+        
+    </div>
+    </div>
+</body>
+</html>`
+}
+
     
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(conten));
 });
 
 app.get('/article-two', function (req, res) {
