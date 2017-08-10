714c344e7251d6199c35f1f779fc7429f5ecb344
rimathi <rimathivanan@gmail.com>
2017-08-10 10:00:34 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,12 +11,14 @@ app.get('/', function (req, res) {
 app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
-app.get('/article-two', function (req, res) {
-    res.send('This is my secound article');
+
+app.get('/article-one', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
-app.get('/article-three', function (req, res) {
-    res.send('This is my third article');
+app.get('/article-one', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
