b6725423f04024a5346aea3411e3ae8134a35ae0
rimathi <rimathivanan@gmail.com>
2017-08-10 15:45:42 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.send(createTemplate(conten));
+  res.send(createTemplate(content));
 });
 
 app.get('/article-two', function (req, res) {
