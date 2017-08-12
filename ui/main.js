1bee9c9aa69b5d58d5db1b80f3fa89b1414e5dc8
rimathi <rimathivanan@gmail.com>
2017-08-12 10:42:16 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,3 @@
 console.log('Loaded!');
-var element=document.getElementById('main-text');
-element.innerHTML("Welcome raj")
+var element=document.getElementById("main-text");
+element.innerHTML("Welcome raj");
