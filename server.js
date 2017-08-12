24da72d26fde512d4cc1e59fccf29f66e18876ab
rimathi <rimathivanan@gmail.com>
2017-08-12 09:32:20 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 var articles=
 {
-'article-One':{
+'article-one':{
     title:'Article one  Mathivanan',
     heading:'Article one',
     date:'Aug 10,2016',
