c7acfde23dddce9c81726934246c8a3f5d8ac2e5
rimathi <rimathivanan@gmail.com>
2017-08-12 10:57:51 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,10 @@
 console.log('Loaded!');
 var element=document.getElementById("main-text");
 element.innerHTML="Welcome raj";
+
+var img=document.getElementById("modi");
+
+img.onclick=function()
+{
+img.style.marginLeft='100px';
+};
