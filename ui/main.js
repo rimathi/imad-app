f267b8c4f2d3f307b0bde61b1d1a2f654d5268e5
rimathi <rimathivanan@gmail.com>
2017-08-14 15:51:33 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,21 @@ var counter=0;
 var button=document.getElementById("counter");
 button.onclick=function()
 {
-counter=counter+1;
-var span=document.getElementById("count");
-span.innerHTML=counter.toString();
+request=XMLHttpRequest();
+
+request.onReadyStateChange=function()
+{
+    if(request.readyState===XMLHttpRequest.DONE)
+    {
+    if(request.status===200)
+    {
+        var counter=request.resposeText;
+        var span=document.getElementById("count");
+        span.innerHTML=counter.toString();
+    }
+    }
+   };
+   request.open("Get","http://rimathivanan.imad.hasura-app.io/counter");
+   request.send(null);
 };
+
