437d7a547e7fb718ce21d23cd554b0f4f682e417
rimathi <rimathivanan@gmail.com>
2017-08-15 14:07:45 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,6 +28,7 @@ var nam=nameInput.value;
 var submit=documnet.document.getElementById("submit_btn");
 submit.onclick=function()
 {
+    alert("I am working");
     var names=["name1","name2","name3","name3"];
     var List='';
     for(i=0;i<names.length;i++)
