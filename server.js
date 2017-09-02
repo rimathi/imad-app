b8082831e48c000757d487df1962315460016163
rimathi <rimathivanan@gmail.com>
2017-09-02 10:22:32 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
+var config={
+user:"rimathivanan",
+database:"rimathivanan",
+host:"db.imad.hasura-app.io",
+port:"5432",
+password:process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -102,6 +110,25 @@ return htmltemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+
+
+var pool=new Pool(config);
+app.get('test-db',function(req,res){
+//make a select request
+//return response with the results
+pool.query("SELECT * FROM test",function(err,result)
+{
+if(err)
+{
+res.status(500).send(err.toString());
+}
+else
+{
+res.send(JSON.stringify(result));
+}
+});
 });
 
 var names[];
