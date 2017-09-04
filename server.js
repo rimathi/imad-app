eb10c5ad0791518ee036382a77e7e6d2d0423031
rimathi <rimathivanan@gmail.com>
2017-09-04 09:09:20 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.get('/articles/:articleName', function (req, res)
 {
  
 
-pool.query("SELECT * FROM article WHERE title=$1",[req.params.articleName] +"'", function(err,result)
+pool.query("SELECT * FROM article WHERE title=$1",[req.params.articleName] , function(err,result)
 {
 
 if(err)
