a663fdda36c1ed471b47694cc8ac7cb95e54ede3
rimathi <rimathivanan@gmail.com>
2017-09-06 10:59:16 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.get('/', function (req, res) {
 function hash(input,salt)
 {
    var hashed=crypto.pbkdf2Sync(input,salt,100000, 512, 'sha512')
-   return (["pbkdf2","1000",salt,hashed.toString('hex')].joint($));
+   return ["pbkdf2","1000",salt,hashed.toString('hex')].join($);
 }
 
 
