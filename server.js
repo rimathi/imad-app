facacf0719389e0de3fdc0853399b7629ad26443
rimathi <rimathivanan@gmail.com>
2017-09-08 15:28:17 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.post('/create-user',function(req,res)
    var salt= crypto.randomBytes(128).toString("hex");
    var dbString=hash(password,salt)
    
-   pool.query('insert into "user" (username,password) values ($1,$2)',[username,dbstring],function()
+   pool.query('insert into "user" (username,password) values ($1,$2)',[username,dbString],function()
    {
        
        if(err)
