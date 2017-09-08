1439e4659ba1ff45a5a5a454d321f7fbab1bf332
rimathi <rimathivanan@gmail.com>
2017-09-08 16:15:30 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.post('/create-user',function(req,res)
    var salt= crypto.randomBytes(128).toString("hex");
    var dbString=hash(password,salt)
    
-   pool.query('insert into "user" (username,password) values ($1,$2)',[username,dbString],function()
+   pool.query('insert into "user" (username,password) values ($1,$2)',[username,dbString],function(err ,result)
    {
        
        if(err)
@@ -100,6 +100,46 @@ res.status(500).send(err.toString());
 else
 {
 res.send("The User has been created succesfully:"+username);
+}
+       
+       
+   });
+    
+});
+
+
+
+app.post('/login',function(req,res)
+{
+    var username=req.body.username;
+    var password=req.body.password;
+      pool.query('select * from "user" username=$1',[username],function(err ,result)
+   {
+       
+       if(err)
+{
+res.status(500).send(err.toString());
+}
+else
+{
+    if(result.rows.length===0)
+    {
+        res.send(403).send("User name or password is invalid");
+    }
+    else
+    {
+        var dbString=result.rows[0].password;
+        var salt=dbString.split('$')[2];
+        var hashPassword=hash(password,salt)
+        if(hashPassword===dbString)
+        {
+        res.send("credential is correct")
+        }
+        else
+        {
+   res.send("User name or password is invalid");
+        }
+}
 }
        
        
