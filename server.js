a7bebfcb53c34c66a8dee6e59752d5d543735b93
rimathi <rimathivanan@gmail.com>
2017-09-09 12:03:46 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,8 +18,7 @@ app.use({
     secret:"SomeRandaomSecretVale",
     cookie:{maxAge:1000*60*60*24*30}
     
-}
-    )
+});
 app.use(bodyparser.json());
 app.use(morgan('combined'));
     function createTemplate(data)
